3566d0ec020d608f64de7091e5a2099abdc42b06
Izaac Molina <39788504+IzMo2000@users.noreply.github.com>
2023-06-28 11:21:06 -0700

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -1,7 +1,6 @@
 import unittest
 from search_feature import get_result_value
 
-
 class searchFeature(unittest.TestCase):
     def test_getResultValue(self):
         self.assertEqual(get_result_value("Batman"), None)
