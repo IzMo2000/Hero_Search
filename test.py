d1a4e9b9fb44626f3bb41e117d79b30f4feb242b
Izaac Molina <izaac.6.30@gmail.com>
2023-06-29 23:40:52 +0000

    added unit tests

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-file_path = './hero_data.db'
-
-try:
-    os.remove(file_path)
-    print(f"Successfully deleted {file_path}")
-except Exception as e:
-    print(f"Error occurred while deleting {file_path}: {e}")
