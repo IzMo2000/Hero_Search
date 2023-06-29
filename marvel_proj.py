549102e0bc987533c971933cc24c7159715d03b5
David <daveozzy6@gmail.com>
2023-06-29 13:27:00 -0500

    added comments with extra functions

--- marvel_proj.py
+++ marvel_proj.py
@@ -1,14 +1,19 @@
-from search_feature import search, print_hero_data, re_prompt, options
+from search_feature import options
 from marvel import Marvel
 from keys import MARVEL_PUBLIC, MARVEL_PRIVATE
-import requests
-import pandas as pd
-import sqlalchemy as db
 
+
+BASE_URL = "https://gateway.marvel.com:443/v1/public/"
+URL = 'https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=ju&apikey=65b63a958c2733164ab372a2d69e038b'
 
 # initialize marvel API
 marvel_data = Marvel(MARVEL_PUBLIC, MARVEL_PRIVATE)
+
 character_data = marvel_data.characters
+
+
+def get_character_json(character_name):
+    return character_data.all(name=character_name)
 
 
 def main():
