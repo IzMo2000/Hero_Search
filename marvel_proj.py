126bb82d2276ddeecc01ed144b644393a478d33e
Izaac Molina <39788504+IzMo2000@users.noreply.github.com>
2023-06-29 22:18:38 -0700

    Update marvel_proj.py

--- marvel_proj.py
+++ marvel_proj.py
@@ -2,10 +2,6 @@ from search_feature import options
 from marvel import Marvel
 from keys import MARVEL_PUBLIC, MARVEL_PRIVATE
 
-
-BASE_URL = "https://gateway.marvel.com:443/v1/public/"
-URL = 'https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=ju&apikey=65b63a958c2733164ab372a2d69e038b'
-
 # initialize marvel API
 marvel_data = Marvel(MARVEL_PUBLIC, MARVEL_PRIVATE)
 
